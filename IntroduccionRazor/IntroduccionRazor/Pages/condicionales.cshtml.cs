a751a71995f859e29b72c1265e371bd3cece9bce
DESKTOP-206NPVK\angel <angro1212@gmail.com>
2024-05-17 19:42:00 -0600

    xx

---
+++ IntroduccionRazor/IntroduccionRazor/Pages/condicionales.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntroduccionRazor.Pages
+{
+    public class condicionalesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
